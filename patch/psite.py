8ae442e1bdb3bc0ac7e98debba2945f6745e0d38
Sorawee Porncharoenwase <nullzero.free@gmail.com>
2013-04-11 02:08:10 +0700

    implement new editpage

--- patch/psite.py
+++ patch/psite.py
@@ -3,6 +3,8 @@
 import init
 from pywikibot.site import *
 
+_logger = "wiki.site"
+
 #=======================================================================
 
 def _getRedirectText(self, text):
@@ -38,5 +40,172 @@ def _allusers(self, start="!", prefix="", group=None, onlyActive=False,
     return augen
 
 APISite.allusers = _allusers
+
+#=======================================================================
+
+@must_be(group='user')
+def _editpage(self, page, summary, minor=True, notminor=False,
+             bot=True, recreate=True, createonly=False, watch=None):
+    """Submit an edited Page object to be saved to the wiki.
+
+    @param page: The Page to be saved; its .text property will be used
+        as the new text to be saved to the wiki
+    @param token: the edit token retrieved using Site.token()
+    @param summary: the edit summary (required!)
+    @param minor: if True (default), mark edit as minor
+    @param notminor: if True, override account preferences to mark edit
+        as non-minor
+    @param recreate: if True (default), create new page even if this
+        title has previously been deleted
+    @param createonly: if True, raise an error if this title already
+        exists on the wiki
+    @param watch: Specify how the watchlist is affected by this edit, set
+        to one of "watch", "unwatch", "preferences", "nochange":
+        * watch: add the page to the watchlist
+        * unwatch: remove the page from the watchlist
+        * preferences: use the preference settings (Default)
+        * nochange: don't change the watchlist
+    @param botflag: if True, mark edit with bot flag
+    @return: True if edit succeeded, False if it failed
+
+    """
+    text = page.text
+    if not text:
+        raise Error("editpage: no text to be saved")
+    if hasattr(page, "_revid"):
+        lastrev = page._revid
+    else:
+        lastrev = None
+    token = self.getToken("edit")
+    self.lock_page(page)
+    params = dict(action="edit",
+                  title=page.title(withSection=False),
+                  text=text, token=token, summary=summary)
+    if bot:
+        params["bot"] = ""
+    if lastrev is not None:
+        params["basetimestamp"] = page._revisions[lastrev].timestamp
+    if minor:
+        params['minor'] = ""
+    elif notminor:
+        params['notminor'] = ""
+    if recreate:
+        params['recreate'] = ""
+    if createonly:
+        params['createonly'] = ""
+    if watch in ["watch", "unwatch", "preferences", "nochange"]:
+        params['watchlist'] = watch
+    elif watch:
+        pywikibot.warning(
+            u"editpage: Invalid watch value '%(watch)s' ignored."
+              % locals())
+## FIXME: API gives 'badmd5' error
+##        md5hash = md5()
+##        md5hash.update(urllib.quote_plus(text.encode(self.encoding())))
+##        params['md5'] = md5hash.digest()
+    req = api.Request(site=self, **params)
+    while True:
+        try:
+            result = req.submit()
+            pywikibot.debug(u"editpage response: %s" % result,
+                            _logger)
+        except api.APIError, err:
+            self.unlock_page(page)
+            if err.code.endswith("anon") and self.logged_in():
+                pywikibot.debug(
+u"editpage: received '%s' even though bot is logged in" % err.code,
+                                _logger)
+            errdata = {
+                'site': self,
+                'title': page.title(withSection=False),
+                'user': self.user(),
+                'info': err.info
+            }
+            if err.code == "badtoken":
+                req["token"] = self.getToken("edit", force=True)
+            if err.code == "spamdetected":
+                raise SpamfilterError(self._ep_errors[err.code] % errdata
+                        + err.info[ err.info.index("fragment: ") + 9: ])
+
+            if err.code == "editconflict":
+                raise EditConflict(self._ep_errors[err.code] % errdata)
+            if err.code in ("protectedpage", "cascadeprotected"):
+                raise LockedPage(errdata['title'])
+            if err.code in self._ep_errors:
+                raise Error(self._ep_errors[err.code] % errdata)
+            pywikibot.debug(
+                u"editpage: Unexpected error code '%s' received."
+                    % err.code,
+                _logger)
+            raise
+        assert ("edit" in result and "result" in result["edit"]), result
+        if result["edit"]["result"] == "Success":
+            self.unlock_page(page)
+            if "nochange" in result["edit"]:
+                # null edit, page not changed
+                pywikibot.log(u"Page [[%s]] saved without any changes."
+                                % page.title())
+                return True
+            page._revid = result["edit"]["newrevid"]
+            # see http://www.mediawiki.org/wiki/API:Wikimania_2006_API_discussion#Notes
+            # not safe to assume that saved text is the same as sent
+            self.loadrevisions(page, getText=True)
+            return True
+        elif result["edit"]["result"] == "Failure":
+            if "captcha" in result["edit"]:
+                captcha = result["edit"]["captcha"]
+                req['captchaid'] = captcha['id']
+                if captcha["type"] == "math":
+                    req['captchaword'] = input(captcha["question"])
+                    continue
+                elif "url" in captcha:
+                    webbrowser.open(captcha["url"])
+                    req['captchaword'] = cap_answerwikipedia.input(
+"Please view CAPTCHA in your browser, then type answer here:")
+                    continue
+                else:
+                    self.unlock_page(page)
+                    pywikibot.error(
+                u"editpage: unknown CAPTCHA response %s, page not saved"
+                                      % captcha)
+                    return False
+            else:
+                self.unlock_page(page)
+                pywikibot.error(u"editpage: unknown failure reason %s"
+                                  % str(result))
+                return False
+        else:
+            self.unlock_page(page)
+            pywikibot.error(
+u"editpage: Unknown result code '%s' received; page not saved"
+                               % result["edit"]["result"])
+            pywikibot.log(str(result))
+            return False
+
+APISite.editpage = _editpage
+
+#=======================================================================
+
+def _getToken(self, tokentype, force=False):
+    """Return token retrieved from wiki to allow changing page content.
+
+    @param tokentype: the type of token (e.g., "edit", "move", "delete");
+        see API documentation for full list of types
+    @param force: force to get new token
+    @type force bool
+
+    """
+    if (not force) and (tokentype in self._token):
+        return self._token[tokentype]
+
+    query = api.PropertyGenerator("info",
+                                  titles="!",
+                                  intoken=tokentype,
+                                  site=self)
+    for item in query:
+        self._token[tokentype] = item[tokentype + "token"]
+        return self._token[tokentype]
+
+APISite.getToken = _getToken
 
 #=======================================================================
--- scripts/test.py
+++ scripts/test.py
@@ -7,13 +7,20 @@ __author__ = "Sorawee Porncharoenwase"
 import init
 import wp
 import pywikibot
+from wp import lthread
 
 def glob():
     pass
 
 def main():
-    page = wp.Page(u"วิกิพีเดีย:ทดลองเขียน")
-    page.delete(reason="abcdef", prompt=False, mark=True)
+    l = wp.conf.sandboxPages
+    pool = lthread.ThreadPool(10)
+    for i in xrange(20):
+        page = wp.Page(l[i % len(l)])
+        page.text = "abcdef " * (10 + i)
+        pool.add_task(site.editpage, page, "test")
+        print "put " + page.title()
+    pool.wait_completion()
 
 if __name__ == "__main__":
     args, site, conf = wp.pre("test")
