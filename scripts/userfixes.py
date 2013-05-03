3b2938fbd7b33efc30fbad645a53bc81b9e0ada8
Sorawee Porncharoenwase <nullzero.free@gmail.com>
2013-05-03 22:35:46 +0700

    fix userfixes

--- scripts/userfixes.py
+++ scripts/userfixes.py
@@ -9,6 +9,7 @@ import pywikibot
 from wp import lre
 
 def glob():
+    global subst
     subst = lre.subst()
     subst.append((u"(?<!วัด)ทรง(เสวย|ประชวร|มีพระ|เป็นพระ|เสด็จ|บรรทม|ผนวช|ทอดพระเนตร|สวรรคต)", r"\1"))
     subst.append((u"== *แหล่งอื่น *==", u"== แหล่งข้อมูลอื่น =="))
@@ -18,13 +19,15 @@ def fix(s):
 
 def main():
     for page in site.allpages(filterredir=False, content=True):
+        #page = wp.Page(u"รายชื่อวัดในจังหวัดชัยนาท")
         text = fix(page.get())
         if page.get() != text:
             pywikibot.showDiff(page.get(), text)
             page.put(text, u"โรบอต: แก้ไขคำผิด", async=True)
+        break
 
 if __name__ == "__main__":
-    args, site, conf = wp.pre(u"user-fixes", lock=True)
+    args, site, conf = wp.pre(u"user-fixes")
     try:
         glob()
         main()
