682a38887dcc635d1e31c8dedc6df5ff8a8f9c21
Sorawee Porncharoenwase <nullzero.free@gmail.com>
2013-06-04 22:17:37 +0700

    fix Timestamp

--- patch/patch.py
+++ patch/patch.py
@@ -1,10 +1,1 @@
 import init
-from pywikibot.page import *
-
-def _editTime(self):
-    rev = self.latestRevision()
-    if rev not in self._revisions:
-        self.site.loadrevisions(self)
-    return pywikibot.Timestamp.fromISOformat(self._revisions[rev].timestamp)
-
-Page.editTime = _editTime
